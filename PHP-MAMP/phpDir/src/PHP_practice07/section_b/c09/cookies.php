41bfccfde79c177477ce09904f1d912f10e9cbd7
Randolph <86254950+RR2894@users.noreply.github.com>
2024-04-16 14:08:28 +0300

    added cookies.php

--- PHP-MAMP/phpDir/src/PHP_practice07/section_b/c09/cookies.php
+++ PHP-MAMP/phpDir/src/PHP_practice07/section_b/c09/cookies.php
@@ -1,23 +1,19 @@
 <?php session_start();
-/*
-Overall idea here is to set cookies and count the number of pages a visitor has viewed.
 
-Step 1: Create a counter to store number of pages vistor has viewed. Create a cookie to hold 
-that counter variable. 
+// Overall idea here is to set cookies and count the number of pages a visitor has viewed.
 
-Step 2: Increment the value of counter, as the visitor has just viewed a page.
+// Step 1: Create a counter to store number of pages vistor has viewed. Create a cookie to hold that counter variable. 
+$counter = $_COOKIE['counter'] ?? 0;
 
-Step 3: Use a setcookie() function to tell the browser to create or update a cookie
-and also store the value from the variable in that cookie
+// Step 2: Increment the value of counter, as the visitor has just viewed a page.
+$counter++;
 
-Step 4: A new variable should hold message saying the number of pages the visitor has viewed.
-e.g. "Page views: "
+// Step 3: Use a setcookie() function to tell the browser to create or update a cookie and also store the value from the variable in that cookie.
+setcookie('counter', $counter);
 
-*/
-
-static $counter = 0;
-$cookie_counter = setcookie($counter);
-
+// Step 4: A new variable should hold message saying the number of pages the visitor has viewed. e.g. "Page views: "
+$message = 'You have visited this site' . $counter . 'times.';
+echo $message;
 
 ?>
 
